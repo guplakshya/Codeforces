f59f42d4d122f75844d97f1e7f7131fd7b7103a3
Lakshya Porwal <42773640+guplakshya@users.noreply.github.com>
2021-10-15 14:53:49 +0530

    Add files via upload

---
+++ Football.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Football
+
+int main(){
+	string s;
+    cin >> s;
+    int contiguous = 1;
+    for (size_t i = 1; i < s.length(); ++i)
+    {
+        if (s[i] == s[i - 1])
+        {
+            contiguous += 1;
+            if (contiguous == 7)
+            {
+                cout << "YES" << endl;
+                return 0;
+            }
+        }
+        else
+        {
+            contiguous = 1;
+        }
+    }
+
+    cout << "NO" << endl;
+	return 0;
+}
