3bbc32ba92cb161eace2ac2d37c3b41e991178d7
Lakshya Porwal <42773640+guplakshya@users.noreply.github.com>
2021-10-16 17:01:27 +0530

    Add files via upload

---
+++ Gravity Flip.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Gravity Flip
+
+int main(){
+	int n,i;
+	cin>>n;
+	int a[n];
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	sort(a,a+n);
+	for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+	return 0;
+}
