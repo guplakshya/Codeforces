687b4c09e8372f2bf274b920e17dd0252bdf5c30
Lakshya Porwal <42773640+guplakshya@users.noreply.github.com>
2021-10-18 23:56:16 +0530

    Add files via upload

---
+++ Ilya and Bank Account.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Ilya and Bank Account
+
+int main(){
+	int n;
+    cin >> n;
+    if (n >= 0)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        int a = n / 10;
+        int b = n / 100 * 10 + n % 10;
+        cout << max(a, b) << endl;
+    }
+	return 0;
+}
