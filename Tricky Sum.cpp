0ef3715bd89b0ec2064ed65d0ee1b309e59d0b8f
Lakshya Porwal <42773640+guplakshya@users.noreply.github.com>
2021-11-18 08:56:58 +0530

    Add files via upload

---
+++ Tricky Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tricky Sum
+int t;
+__int64 n,sum,sum2,num;
+int main(){
+	cin>>t;
+	for(int i=1;i<=t;i++){
+		cin>>n;
+		num=1;
+		sum2=0;
+		sum = n*(n+1)/2;
+		while(num<=n){
+			sum2+=num;
+			num*=2;
+		}
+		cout<<sum-sum2*2<<endl;
+	}
+	return 0;
+}
